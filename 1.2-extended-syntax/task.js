3b4c552aaff0f33eae6fbc85978ec64294b8bc57
Yushkevich Anton Alekseevich <Mortifer1993@yandex.ru>
2020-12-20 17:55:03 +0300

    first solution for task number one

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,16 +1,23 @@
-'use strict'
+// 'use strict'
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c){
+    let x= [];
+    let discriminant = Math.pow(b, 2) - 4 * a * c;
+    if ( discriminant === 0) {
+        x.push( (-b + Math.sqrt(discriminant)) / (2 * a));
+    } else if (discriminant > 0) {
+        x.push( (-b + Math.sqrt(discriminant)) / (2 * a));
+        x.push( (-b - Math.sqrt(discriminant)) / (2 * a));
+    }
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+// function getAverageMark(marks){
+//     // код для задачи №2 писать здесь
+//     // return averageMark;
+// }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
-}
+// function askDrink(name,dateOfBirthday){
+//     // код для задачи №3 писать здесь
+//     // return result;
+// }
