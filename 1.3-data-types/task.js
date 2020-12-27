dbc45cbaf0a8bf1ac77f78403d770fff1a82b0fe
Yushkevich Anton Alekseevich <Mortifer1993@yandex.ru>
2020-12-27 23:30:30 +0300

    create function for mortgage count

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,8 +1,39 @@
-"use strict"
+'use strict'
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let errorParametr = '';
+    let errorValue = '';
+    let percentValue = +percent;
+    let contributionValue = +contribution;
+    let amountValue = +amount;
+
+    switch (true){
+        case (isNaN(percentValue)):
+        errorParametr = 'проценты';
+        errorValue = percent;
+        break;
+        case (isNaN(contributionValue)):
+        errorParametr = 'первоначальный взнос';
+        errorValue = contribution;
+        break;
+        case (isNaN(amountValue)):
+        errorParametr = 'сумма кредита';
+        errorValue = amount;
+        break;
+    }
+
+    if(errorParametr || errorValue) {
+        return `Параметр ${errorParametr} содержит неправильное значение "${errorValue}"`
+    }
+
+    let bodyOfTheLoan = amountValue - contributionValue;
+    let today = new Date();
+    let deadlineCredit = (date.getFullYear() - today.getFullYear()) * 12 - today.getMonth() + date.getMonth();
+    let monthlyPercentage = percentValue / 100 / 12;
+    let monthlyPayment = bodyOfTheLoan * (monthlyPercentage + monthlyPercentage / (((1 + monthlyPercentage) ** deadlineCredit) - 1));
+    let totalAmount = Math.round(monthlyPayment * deadlineCredit * 100) / 100;
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
