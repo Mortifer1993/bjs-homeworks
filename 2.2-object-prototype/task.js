0c248a34e1350d2e17ad0a7982f1209e9544b712
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2020-12-31 12:31:21 +0300

    function getAverageMark added

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,3 +1,5 @@
+'use strict'
+
 String.prototype.isPalindrome = function(){
     let newString = this.toLowerCase().split(' ').join('');
     for(let i = newString.length - 1, a = 0; i >= 0 ; i--){
@@ -10,8 +12,19 @@ String.prototype.isPalindrome = function(){
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let totalSum = 0;
+
+    if(marks.length === 0){
+        return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++){
+        totalSum += marks[i];
+    }
+
+    let averageMark = totalSum / marks.length;
+    let roundedAverage = Math.round(averageMark)
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
