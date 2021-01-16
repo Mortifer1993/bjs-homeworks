c0d818eef1056be55a76ea430368944144709814
Yushkevich Anton Alekseevich <Mortifer1993@yandex.ru>
2021-01-16 15:44:22 +0300

    first task in homework"oop" was complited

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -17,7 +17,7 @@ class PrintEditionItem {
     set state(value) { 
         if (value >= 100) {
             this._state = 100;
-        }else if( vlue <= 0){
+        }else if( value <= 0){
             this._state = 100;
         } else {
             this._state = value;
@@ -29,8 +29,38 @@ class PrintEditionItem {
     }
 }
 
-class Magazine extends PrintEditionItem{
-    constructor(type, ...rest) {
+class Magazine extends PrintEditionItem {
+    constructor(...rest) {
         super(...rest);
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, ...rest) {
+        super(...rest);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+
+class NovelBook extends Book {
+    constructor(...rest) {
+        super(...rest);
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(...rest) {
+        super(...rest);
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(...rest) {
+        super(...rest);
+        this.type = 'detective';
     }
 }
